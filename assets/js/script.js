ef5899d0473d188ec96de9051e0e9dbbca47449d
Luke Cole <Lukeajcole@gmail.com>
2021-07-13 15:55:22 -0700

    Initial Commit

---
+++ assets/js/script.js
@@ -0,0 +1,131 @@
+var mainContainer = $('#schedContainer');
+var currTime = $('#currentDay');
+
+var startHour = 16;
+var endHour = 24;
+var currHour = moment().format("HH");
+
+var i;
+
+var noteStore = initializeStorage();
+var scheduleEl = makeScheduleEl(noteStore);
+mainContainer.append(scheduleEl);
+
+//Set Today's date
+var today = moment().format("[Today is] LL")
+currTime.text(today);
+
+noteStore['date'] = moment().format("YYYYMMDD");
+
+setItemState();
+startTimer();
+
+
+
+
+mainContainer.on('click', saveInput);
+
+
+/////////////////////////////////////////
+
+function saveInput (event) {
+    var target = $(event.target);
+
+    if (target.is("button")) {
+        itemNote = target.parent().siblings('#noteEl').children().children().val();
+        itemHour = 'h' + target.parent().siblings('#hourLbl').text();
+        noteStore[itemHour] = itemNote;
+        localStorage.setItem('noteStore', JSON.stringify(noteStore));
+    }
+}
+
+function makeScheduleEl (noteStore) {
+    var scheduleEl = $("<div>");
+
+    for (i=startHour; i < endHour; i++){
+        //create the row element
+        var itemEl = $('<tr>');
+        itemEl.attr('id','h' + i);
+        itemEl.addClass('row border-top border-bottom');
+        //create and append hour label
+        var hourEl = $('<td>');
+        hourEl.text(i);
+        hourEl.addClass('col-sm-1 text-center align-middle');
+        hourEl.attr('id','hourLbl');
+        //create and append note element
+        var noteEl = $('<td>');
+        noteEl.attr('id','noteEl');
+            //create and append form element
+            var noteForm = $('<div>');
+            noteForm.addClass('form-group align-items-center');
+            var noteInput = $('<textarea>');
+            noteInput.addClass('form-control m-2');
+            noteInput.text(noteStore['h' + i]);
+            noteForm.append(noteInput);
+            noteEl.append(noteForm);
+        
+        noteEl.addClass('col-sm-10 border-left');
+        //create and append save button
+        var btnEl = $('<td>');
+        btnEl.addClass('col-sm-1 border-left text-center align-middle');
+        var saveBtn = $("<button>");
+        saveBtn.type = "button";
+        saveBtn.addClass("btn btn-primary saveBtn m-3");
+        var iEl = $('<i>');
+        iEl.text("SAVE");
+        saveBtn.append(iEl);
+        btnEl.append(saveBtn);
+
+        itemEl.append(hourEl, noteEl, btnEl);
+        scheduleEl.append(itemEl);
+    }
+    
+    return scheduleEl;
+}
+
+{/* <div class="form-group">
+<textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div> */}
+
+function initializeStorage() {
+    var i;
+    var storeChk = localStorage.getItem('noteStore');
+    if (storeChk === null){
+        var noteStore = {date:"20210711"};
+        for (i=0; i < 24; i++){
+            noteStore["h" + i] = '';
+        }
+        localStorage.setItem('noteStore', JSON.stringify(noteStore));
+    } else {
+        var noteStore = JSON.parse(localStorage.getItem('noteStore'));
+    }
+    return noteStore;
+}
+
+function startTimer (){
+    interval = setInterval(function(){
+        currHour = moment().format("HH");
+        setItemState();
+    }, 10000)
+}
+
+function setItemState (){
+    for (i=startHour; i < endHour; i++){
+        var currEl = $('#h' + i);
+        if (i < currHour) {
+            // set class = past
+            currEl.addClass('past');
+            currEl.removeClass('present future')
+        } else if (i == currHour) {
+            //set class = present
+            currEl.addClass('present');
+            currEl.removeClass('past future')
+        } else if (i > currHour) {
+            //set class = future
+            currEl.addClass('future');
+            currEl.removeClass('present past')
+        }
+
+    }
+
+}
